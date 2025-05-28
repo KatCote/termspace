72658590461aa26ea7e1724994692b24499da3ee
Stepan Pukhovskiy <stepan.puhovskiy@tmhsmart.ru>
2025-05-29 01:04:54 +0300

    Tabs refactor

--- main.c
+++ main.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-		int retcode = 0;
+	int retcode = 0;
 
-		if ((retcode = ncurses_init()) != 0)
-		{ return -1; }
+	if ((retcode = ncurses_init()) != 0)
+	{ return -1; }
     
     clear();
  
     int row, col;
     getmaxyx(stdscr, row, col);
 		
-		render_bg_grid(row, col);
+	render_bg_grid(row, col);
 
-		ncurses_final();
+	ncurses_final();
 
     return 0;
 }
--- render.c
+++ render.c
@@ -9,27 +9,27 @@ const int cell_size = 10;
 
 int render_thread()
 {
-		return 0;
+	return 0;
 }
 
 void render_bg_grid(int rows, int cols)
 {
-		attron(COLOR_PAIR(1));
+	attron(COLOR_PAIR(1));
 
-		for (int c = 0; c < (cols * coef); c++) {
-				for (int r = 0; r < rows; r++) {
-						if ((r+pos_y)%cell_size == 0 ) { mvprintw(r, c, "%s", s_hor); }
-						if ((c+pos_x)%(cell_size*coef) == 0 ) { mvprintw(r, c, "%s", s_vert); }
-						if ((r+pos_y)%cell_size == 0 && (c+pos_x)%(cell_size*coef) == 0) { mvprintw(r, c, "%s", s_ct); }
-				}
+	for (int c = 0; c < (cols * coef); c++) {
+		for (int r = 0; r < rows; r++) {
+			if ((r+pos_y)%cell_size == 0 ) { mvprintw(r, c, "%s", s_hor); }
+			if ((c+pos_x)%(cell_size*coef) == 0 ) { mvprintw(r, c, "%s", s_vert); }
+			if ((r+pos_y)%cell_size == 0 && (c+pos_x)%(cell_size*coef) == 0) { mvprintw(r, c, "%s", s_ct); }
 		}
+	}
 
-		attroff(COLOR_PAIR(1));
+	attroff(COLOR_PAIR(1));
 }
 
 void pairs_init()
 {
-		init_pair(1, 8, -1);
+	init_pair(1, 8, -1);
 }
 
 int ncurses_init()
@@ -39,26 +39,26 @@ int ncurses_init()
     initscr();
 
     if (!has_colors())
-		{  
+	{  
         endwin();
         printf("Term colors not supported. Exiting.\n");
 
-				return -1;
+		return -1;
     }
-		else
-		{
-				start_color();  
-				use_default_colors();  
-		}
+	else
+	{
+		start_color();  
+		use_default_colors();  
+	}
 
     cbreak(); 
     noecho();
     keypad(stdscr, TRUE); 
-		curs_set(0);
+	curs_set(0);
 
-		pairs_init();
+	pairs_init();
 
-		return 0;
+	return 0;
 }
 
 void ncurses_final()
